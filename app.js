1760d44e385ddd99042945f555e7c3ef58f9645a
Darth Vader <vador@axa-tech.com>
2017-11-18 00:05:47 +0000

    Extended app.js to connect to a device / characteristic (work in progress)

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ noble.on('stateChange', function(state) {
     noble.stopScanning();
 });
 
+var charList = []
+
 noble.on('discover', function(peripheral) {  
   console.log('discovered', peripheral.advertisement.localName);
 
@@ -24,14 +26,49 @@ noble.on('discover', function(peripheral) {
     peripheral.connect(function(error) {
       if (error) { console.log('error during connection', error); }
       else {
-        peripheral.on('disconnect', function() {
-          console.log("disconnected", peripheral.advertisement.localName)
-        }); 
+        console.log("connected", peripheral.advertisement.localName)        
+
+        peripheral.discoverAllServicesAndCharacteristics(function(error, services, characteristics){
+          //console.log('discovered services & characteristics', error, services, characteristics);
+
+          if (error) {
+            console.log('an error occured during discovery', error);
+          }
+
+          for (let i = 0; i < characteristics.length; i++) {
+            var c = characteristics[i];
+            if (c.properties.indexOf('indicate') !== -1) {
+              console.log('discovered characteristic', c.name, c.uuid, c.properties);
+
+              charList.push(c);
+              subscribe(charList.length - 1);
+            }
+          }
+
+        });
       }
     });
 
   }
 });
+
+const subscribe = (idx) => {
+  var c = charList[idx];
+
+  c.once('notify', function(state){ console.log('notify change for', c.name, c.uuid, c.properties, state); });
+
+  c.on('data', function(data, isNotification){
+    console.log('received data', data, 'for', c.name, c.uuid);
+  });
+
+  c.subscribe(function(error){ 
+    console.log('subscribed', c.name, c.uuid, c.properties, error); 
+  });
+
+  c.notify(true, function(error) { 
+    console.log('notified', c.name, c.uuid, c.properties, error); 
+  });
+};
 
 noble.on('scanStart', function() {
   console.log('scan start');
